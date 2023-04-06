72915f2699f0dc9b84c3af189b5a3338ce091ae1
unknown <mykael.barnes@hotmail.com>
2023-04-06 14:33:40 -0700

    modal added

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,7 @@
-import { saveAs } from 'file-saver';
+
 
 export default function Modal(props: {imgSource: string, setSelectedImage: React.Dispatch<React.SetStateAction<string>>}){
 
-    let blob = new Blob([props.imgSource], {type: 'image/png'})
-
-    console.log(URL.createObjectURL(blob))
-    // console.log(props.imgSource)
-    console.log(blob)
 
     return(
         <div className="modal-background">
